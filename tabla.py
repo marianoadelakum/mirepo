39a6b6c0830a73ef9c6983162c8b1474be69ac50
marianoadelakum <marianoadelakum@gmail.com>
2022-02-01 09:36:03 +0100

    misegunda version tabla

--- tabla.py
+++ tabla.py
@@ -11,3 +11,9 @@ print("0 and 1=", 0 and 1)
 print("1 and 0=", 1 and 0)
 print("1 and 1=", 1 and 1)
 
+print("Tabla or")
+print("0 or 0=", 0 or 0)
+print("0 or 1=", 0 or 1)
+print("1 or 0=", 1 or 0)
+print("1 or 1=", 1 or 1)
+
