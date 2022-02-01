9d63728070d9b8db104909519641789212632d71
marianoadelakum <marianoadelakum@gmail.com>
2022-02-01 09:29:14 +0100

    mi primera version tabla

---
+++ tabla.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  7 10:47:13 2021
+
+@author: MarianoDjemilAdelaku
+"""
+
+print("Tabla and")
+print("0 and 0=", 0 and 0)
+print("0 and 1=", 0 and 1)
+print("1 and 0=", 1 and 0)
+print("1 and 1=", 1 and 1)
+
