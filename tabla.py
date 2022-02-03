409bcad0b392eb1019b138a79e6831c7b4bb3e4d
suaveboy77 <marianoadelakum@gmail.com>
2022-02-03 13:12:21 +0100

    mergeo de tablas

--- tabla.py
+++ tabla.py
@@ -29,7 +29,6 @@ print("0 or 1=",bool(0 or 1))
 print("1 or 0=",bool(1 or 0))
 print("1 or 1=",bool(1 or 1))
 
-<<<<<<< HEAD
 print("vamos a calcular el IMC")
 
 altura=float(input("introduce tu altura: "))
@@ -37,7 +36,6 @@ peso=float(input("introduce tu peso: "))
 resultado=peso/(altura*altura)
 
 print("tu IMC es: ",resultado)
-=======
 if resultado<19:
    print("peso inferior a lo normal")
 elif resultado >=19 and resultado<=25:
@@ -46,6 +44,5 @@ elif resultado >=25 and resultado<=30:
     print("peso superior al normal")
 elif resultado>30:
     print("obesidad")
->>>>>>> newram
 
 print("vamos a probar el conflicto con mi carpeta de trabajo")
